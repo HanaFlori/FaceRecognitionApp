d01a40e5ee05765dbee7601bfeffa6fd84295079
HanaFlori <48555939+HanaFlori@users.noreply.github.com>
2020-05-06 20:31:39 +0300

    Add files via upload

---
+++ precache-manifest.f404836448c00e347dd9135315943458.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e14a1032237d04b2efa47efb2dbf0c0",
+    "url": "/FaceRecognitionApp/index.html"
+  },
+  {
+    "revision": "8638728f09ac3da800a6",
+    "url": "/FaceRecognitionApp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "972c6c3ba6daf2f663f5",
+    "url": "/FaceRecognitionApp/static/css/main.d2317254.chunk.css"
+  },
+  {
+    "revision": "8638728f09ac3da800a6",
+    "url": "/FaceRecognitionApp/static/js/2.f4b37c55.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/FaceRecognitionApp/static/js/2.f4b37c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "972c6c3ba6daf2f663f5",
+    "url": "/FaceRecognitionApp/static/js/main.dca71a2a.chunk.js"
+  },
+  {
+    "revision": "5dffa7e4f136915204c3",
+    "url": "/FaceRecognitionApp/static/js/runtime-main.30b8d365.js"
+  }
+]);
